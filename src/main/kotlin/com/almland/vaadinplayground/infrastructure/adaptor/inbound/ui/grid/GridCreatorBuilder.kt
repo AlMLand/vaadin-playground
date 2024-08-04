d972f40f9d5fc900725f348c06a75a4072aa2cd2
Alex Morland <Alex.Morland@ottogroup.com>
2024-08-04 15:06:28 +0200

    added date time format

--- src/main/kotlin/com/almland/vaadinplayground/infrastructure/adaptor/inbound/ui/grid/GridCreatorBuilder.kt
+++ src/main/kotlin/com/almland/vaadinplayground/infrastructure/adaptor/inbound/ui/grid/GridCreatorBuilder.kt
@@ -3,9 +3,12 @@ package com.almland.vaadinplayground.infrastructure.adaptor.inbound.ui.grid
 import com.almland.vaadinplayground.domain.Todo
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.grid.Grid
+import com.vaadin.flow.data.renderer.LocalDateTimeRenderer
 
 
 internal object GridCreatorBuilder {
+
+    private const val DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm:ss"
 
     fun createTodoGrid(showHideColumnButton: Button): Grid<Todo> =
         Grid(Todo::class.java, false)
@@ -17,7 +20,8 @@ internal object GridCreatorBuilder {
                         .also { menu.addColumnToggleItem(GridColumn.BODY.name, it) }
                     addColumn(Todo::author).setHeader(GridColumn.AUTHOR.name)
                         .also { menu.addColumnToggleItem(GridColumn.AUTHOR.name, it) }
-                    addColumn(Todo::createdAt).setHeader(GridColumn.CREATED.name)
+                    addColumn(LocalDateTimeRenderer(Todo::createdAt, DATE_TIME_FORMAT))
+                        .setHeader(GridColumn.CREATED.name)
                         .also { menu.addColumnToggleItem(GridColumn.CREATED.name, it) }
                     setSelectionMode(Grid.SelectionMode.MULTI)
                 }
